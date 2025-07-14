fec1bc5177f620f89bc206d425e8f9c5c88fcf33
feri vass <francisc.vass@oracle.com>
2025-07-14 18:10:57 +0300

    backend oci

--- terraform/vcn/main.tf
+++ terraform/vcn/main.tf
@@ -1,25 +1,16 @@
-terraform {
-  
-  backend "s3" {
-    bucket = "bucket-TE"
-    region = "us-ashburn-1"
+terraform {  
+  backend "oci" {
+    bucket = "bucket-tag"
     key = "tf_demo_native_backend.tfstate"
-    use_lockfile = true
-    
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    skip_metadata_api_check     = true
-    use_path_style              = true
-
-    endpoints = { s3 = "https://id3kvohtwgjy.compat.objectstorage.us-ashburn-1.oraclecloud.com" } 
+    namespace = "id3kvohtwgjy"
+    config_file_profile = "DEFAULT"
+    auth = "SecurityToken"
+    region = "us-ashburn-1"    
   }
 }
 
 provider "oci" {
   region               = var.region
-  tenancy_ocid         = var.tenancy_ocid
   auth                 = "SecurityToken"
   config_file_profile  = "DEFAULT"
 }
